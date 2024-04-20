33871ec78fca6a1ee551e910057c5c8f4d380f62
Ramana Sree K V <73020282+Edith-Stark06@users.noreply.github.com>
2024-04-21 02:47:40 +0530

    Complaint.sol

--- Complaint.sol
+++ Complaint.sol
@@ -11,9 +11,10 @@ contract ComplaintContract {
         bytes Place;
         uint currentTime; 
         bytes EventTime;
+        string Reason;
     }
     
-    event ComplaintFiled(bytes Email, uint Phone, bytes Name, bytes Suspect_Name, bytes Place, uint currentTime, bytes EventTime);
+    event ComplaintFiled(bytes Email, uint Phone, bytes Name, bytes Suspect_Name, bytes Place, uint currentTime, bytes EventTime, string Reason );
 
     function fileComplaint(
         bytes memory _Email,
@@ -21,8 +22,9 @@ contract ComplaintContract {
         bytes memory _Name,
         bytes memory _Suspect_Name,
         bytes memory _Place,
-        bytes memory _EventTime
+        bytes memory _EventTime,
+        string memory _Reason 
     ) public {
-        emit ComplaintFiled(_Email, _Phone, _Name, _Suspect_Name, _Place, block.timestamp, _EventTime);
+        emit ComplaintFiled(_Email, _Phone, _Name, _Suspect_Name, _Place, block.timestamp, _EventTime, _Reason);
     }
 }
